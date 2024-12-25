1cead848b2f93ef6c44d43288f926c282c47ec16
Ermias Wakgari Yadeta <118937954+ERMIASWAKGARI@users.noreply.github.com>
2024-12-25 05:24:41 -0900

    modified projects section: made slight change on section header

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -4,7 +4,7 @@ export default function Projects() {
   return (
     <section
       id="projects"
-      className="py-10 px-5 sm:px-10 lg:px-20 bg-tertiary dark:bg-tertiary-dark"
+      className="py-20 px-8 sm:px-10 lg:px-20 bg-tertiary dark:bg-tertiary-dark"
     >
       <h2 className="text-3xl sm:text-4xl font-bold text-center text-gray-800 dark:text-neutral-white mb-10">
         Projects
