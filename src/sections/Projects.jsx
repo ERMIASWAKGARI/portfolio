d5758863af0876921888d5679fc0f35d5b5d36d4
Ermias Wakgari Yadeta <118937954+ERMIASWAKGARI@users.noreply.github.com>
2024-12-25 10:28:28 -0900

    modified projects section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -4,9 +4,9 @@ export default function Projects() {
   return (
     <section
       id="projects"
-      className="py-20 px-8 sm:px-10 lg:px-20 bg-tertiary dark:bg-tertiary-dark"
+      className="py-20 px-8 sm:px-10 lg:px-20 bg-gray-100 dark:bg-gray-900"
     >
-      <h2 className="text-3xl sm:text-4xl font-bold text-center text-gray-800 dark:text-neutral-white mb-10">
+      <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center text-gray-800 dark:text-neutral-white mb-5 lg:mb-8">
         Projects
       </h2>
 
