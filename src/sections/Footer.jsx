0dc5cb32dab00c661f2171ac8b70d4542d4e7c8b
Ermias Wakgari Yadeta <118937954+ERMIASWAKGARI@users.noreply.github.com>
2025-01-02 07:57:12 -0900

    implememted footer dark/light mode

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <section id="footer">
-      <footer className="py-6 px-8 bg-gray-900 text-gray-200 text-center">
+      <footer className="py-6 px-8 bg-[#f0f9ff] dark:bg-[#003049]  text-gray-800 dark:text-neutral-white text-center">
         <p className="mt-4">
           © {new Date().getFullYear()} Ermias Wakgari. All rights reserved.
         </p>
