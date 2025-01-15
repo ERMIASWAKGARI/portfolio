77a72bdfe3a9ad1607279175f3ea4c34206f6c41
Ermias Wakgari Yadeta <118937954+ERMIASWAKGARI@users.noreply.github.com>
2025-01-15 02:14:19 -0900

    edited skills section

--- src/sections/Skill.jsx
+++ src/sections/Skill.jsx
@@ -16,80 +16,67 @@ import {
 
 const Skill = () => {
   const frontEndTechs = [
-    { icon: <FaReact color="#61DAFB" />, name: "React", level: 90 },
-    { icon: <SiTypescript color="#3178C6" />, name: "TypeScript", level: 80 },
-    { icon: <FaJs color="#F7DF1E" />, name: "JavaScript", level: 85 },
-    {
-      icon: <SiTailwindcss color="#38B2AC" />,
-      name: "Tailwind CSS",
-      level: 90,
-    },
-    { icon: <FaBootstrap color="#7952B3" />, name: "Bootstrap", level: 70 },
+    { icon: <FaReact color="#61DAFB" />, name: "React" },
+    { icon: <SiTypescript color="#3178C6" />, name: "TypeScript" },
+    { icon: <FaJs color="#F7DF1E" />, name: "JavaScript" },
+    { icon: <SiTailwindcss color="#38B2AC" />, name: "Tailwind CSS" },
+    { icon: <FaBootstrap color="#7952B3" />, name: "Bootstrap" },
   ];
 
   const backEndTechs = [
-    { icon: <FaNode color="#339933" />, name: "Node.js", level: 85 },
-    { icon: <SiMongodb color="#47A248" />, name: "MongoDB", level: 80 },
-    { icon: <SiMysql color="#4479A1" />, name: "MySQL", level: 75 },
-    { icon: <FaPhp color="#777BB4" />, name: "PHP", level: 65 },
-    { icon: <FaServer color="#6C757D" />, name: "Express.js", level: 80 },
+    { icon: <FaNode color="#339933" />, name: "Node.js" },
+    { icon: <SiMongodb color="#47A248" />, name: "MongoDB" },
+    { icon: <SiMysql color="#4479A1" />, name: "MySQL" },
+    { icon: <FaPhp color="#777BB4" />, name: "PHP" },
+    { icon: <FaServer color="#6C757D" />, name: "Express.js" },
   ];
 
   const renderTechList = (title, techs) => (
-    <div className="flex flex-row items-center justify-center w-full space-y-6 gap-6">
+    <div>
       <motion.div
-        className="text-left"
         whileInView={{ opacity: 1, x: 0 }}
         initial={{ opacity: 0, x: -20 }}
         transition={{ duration: 0.5 }}
       >
-        <h3 className="text-xl sm:text-2xl lg:text-3xl font-bold text-gray-800 dark:text-white mb-4">
+        <h3 className="text-xl sm:text-2xl lg:text-3xl font-bold text-gray-800 dark:text-white mb-6 text-center">
           {title}
         </h3>
-        <div className="w-16 sm:w-full h-1 bg-teal-400 dark:bg-primary-light mb-4"></div>
       </motion.div>
-
-      <div className="space-y-4">
+      <motion.div
+        className="flex flex-wrap justify-center gap-6 lg:gap-8"
+        whileInView={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        transition={{ staggerChildren: 0.1, duration: 0.4 }}
+      >
         {techs.map((tech, index) => (
           <motion.div
             key={index}
-            className="flex items-center gap-4"
-            whileInView={{ opacity: 1, x: 0 }}
-            initial={{ opacity: 0, x: -20 }}
+            className="flex flex-col items-center justify-center gap-4"
+            whileInView={{ opacity: 1, scale: 1 }}
+            initial={{ opacity: 0, scale: 0.8 }}
             transition={{ delay: index * 0.1, duration: 0.4 }}
           >
-            <div className="flex items-center justify-center text-xl sm:text-2xl lg:text-3xl">
+            <div
+              className="text-4xl lg:text-5xl p-4 rounded-full"
+              style={{
+                backgroundColor: tech.icon.props.color + "33", // Add transparency for a subtle background
+              }}
+            >
               {tech.icon}
             </div>
-            <div className="flex-grow">
-              <div className="flex justify-between items-center">
-                <p className="text-sm sm:text-base md:text-lg font-medium text-gray-800 dark:text-white">
-                  {tech.name}
-                </p>
-                <p className="text-sm text-gray-500 dark:text-gray-400">
-                  {tech.level}%
-                </p>
-              </div>
-              <div className="w-full bg-gray-200 dark:bg-gray-700 h-2 rounded-md mt-1">
-                <div
-                  className="h-full rounded-md"
-                  style={{
-                    width: `${tech.level}%`,
-                    backgroundColor: tech.icon.props.color,
-                  }}
-                ></div>
-              </div>
-            </div>
+            <p className="text-sm sm:text-base md:text-lg font-medium text-gray-800 dark:text-teal-400">
+              {tech.name}
+            </p>
           </motion.div>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 
   return (
     <section
       id="skills"
-      className="py-10 sm:py-20 px-4 sm:px-8 lg:px-16 bg-gray-50  dark:bg-[#001d3d]"
+      className="py-10 sm:py-20 px-4 sm:px-8 lg:px-16 bg-gray-50 dark:bg-[#001d3d]"
     >
       <motion.div
         whileInView={{ opacity: 1, y: 0 }}
@@ -97,19 +84,16 @@ const Skill = () => {
         transition={{ duration: 0.6 }}
         viewport={{ once: false }}
       >
-        <h2 className="text-xl sm:text-3xl md:text-4xl font-bold text-center text-gray-800 dark:text-neutral-white mb-4 sm:mb-8">
+        <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center text-gray-800 dark:text-white mb-6 sm:mb-12">
           Skills
         </h2>
-
         <p className="text-sm sm:text-base md:text-lg text-center text-secondary-dark dark:text-secondary-light mb-8 sm:mb-16">
           Over the years, I have honed my skills in these technologies.
         </p>
-
         <div className="flex flex-col lg:flex-row lg:space-x-12 space-y-12 lg:space-y-0">
           <div className="flex-1">
             {renderTechList("Frontend", frontEndTechs)}
           </div>
-
           <div className="flex-1">
             {renderTechList("Backend", backEndTechs)}
           </div>
