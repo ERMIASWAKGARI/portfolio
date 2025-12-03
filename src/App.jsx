af9e16896a82502642401da7f0ec6a368a3826e6
Ermias Wakgari <ermiaswakgarisse5@gmail.com>
2025-12-03 16:17:35 +0300

    added vercel speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import GoogleAnalytics from "./components/GoogleAnalytics";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import ScrollIndicator from "./components/ScrollIndicator";
 import ThemeToggle from "./components/ThemeToggle";
 import About from "./sections/About";
@@ -26,6 +27,7 @@ function App() {
       <Contact />
       <Footer />
       <ScrollIndicator />
+      <SpeedInsights />
     </div>
   );
 }
